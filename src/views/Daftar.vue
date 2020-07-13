1d10cbb4d8da6a05c8cecbfa7ae2cb8e26561167
eddogustian <eddogustian78@gmail.com>
2020-07-13 22:12:30 +0700

    daftar

--- src/views/Daftar.vue
+++ src/views/Daftar.vue
@@ -1,3 +1,32 @@
 <template>
-  <h2>halaman daftar</h2>
+  <v-container>
+    <v-layout wrap style="margin-top: 80px;">
+      <v-flex sm12 md6 offset-md3>
+        <v-card>
+          <v-col>
+            <v-row>
+              <v-col md="12">
+                <v-text-field outline label="Nama Lengkap" type="text"></v-text-field>
+                <v-text-field outline label="Email" type="text"></v-text-field>
+                <v-text-field outline label="Password" type="password"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col>
+            <v-row>
+              <v-col md="12">
+                <v-btn color>Daftar</v-btn>
+              </v-col>
+              <v-col>
+                <p>
+                  Sudah Punya Akun ?
+                  <router-link to="/">Login Disini</router-link>
+                </p>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
