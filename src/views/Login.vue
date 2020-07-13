d1df8c5a7172dabddcb5470d1b4487088596b13e
eddogustian <eddogustian78@gmail.com>
2020-07-13 21:59:08 +0700

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,46 @@
 <template>
-  <h2>halaman untuk login</h2>
+  <v-container>
+    <v-layout wrap style="margin-top: 150px;">
+      <v-flex sm12 md6 offset-md3>
+        <v-card>
+          <v-col md="12">
+            <v-row>
+              <v-col md="12">
+                <v-text-field label="Username"></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field label="Password"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col md="12">
+            <v-row>
+              <v-col md="12">
+                <v-btn block color>Login</v-btn>
+              </v-col>
+              <v-col md="12">
+                <v-btn block color @click="redirectDaftar()">Daftar</v-btn>
+              </v-col>
+              <v-col md="12">
+                <p>
+                  Lupa Password ?
+                  <router-link to="lupa-password">Klik Disini</router-link>
+                </p>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirectDaftar() {
+      this.$router.push("/daftar");
+    }
+  }
+};
+</script>
